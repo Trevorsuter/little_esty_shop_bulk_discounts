0782ffa990b48f8fa3c7633526f31b696f6da970
Trevor Suter <Trevorsuter@icloud.com>
2021-03-04 19:33:18 -0700

    changed percentage from float to a decimal

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_05_021841) do
+ActiveRecord::Schema.define(version: 2021_03_05_023152) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "bulk_discounts", force: :cascade do |t|
-    t.float "percentage"
+    t.decimal "percentage", precision: 10, scale: 2
     t.integer "threshold"
     t.bigint "merchant_id"
     t.index ["merchant_id"], name: "index_bulk_discounts_on_merchant_id"
