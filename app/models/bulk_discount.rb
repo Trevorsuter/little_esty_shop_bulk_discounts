5297c480f61d3edd2fe846347b12036849082917
Trevor Suter <Trevorsuter@icloud.com>
2021-03-04 20:13:32 -0700

    took out before save calculation until further notice

--- app/models/bulk_discount.rb
+++ app/models/bulk_discount.rb
@@ -3,9 +3,4 @@ class BulkDiscount < ApplicationRecord
   validates_presence_of :percentage
   validates_presence_of :threshold
   validates_presence_of :description
-  before_save :calculate_percentage
-
-  def calculate_percentage
-    self.percentage *= 0.01
-  end
 end
--- spec/models/bulk_discount_spec.rb
+++ spec/models/bulk_discount_spec.rb
@@ -9,15 +9,4 @@ RSpec.describe BulkDiscount, type: :model do
     it {should validate_presence_of :threshold}
     it {should validate_presence_of :description}
   end
-
-  describe 'before save' do
-    before :each do
-      @merchant = Merchant.create(name: "Trevor Suter")
-      @discount = @merchant.bulk_discounts.create(percentage: 20, threshold: 10, description: "discount")
-    end
-    it 'changes the full integer to a decimal' do
-      expect(@discount.percentage.to_f).to_not eq(20.0)
-      expect(@discount.percentage.to_f).to eq(0.2)
-    end
-  end
 end
