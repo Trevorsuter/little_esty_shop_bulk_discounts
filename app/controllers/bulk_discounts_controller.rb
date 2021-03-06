da0f9b1fc6bb16b72d49c2190d06004d430b5be8
Trevor Suter <Trevorsuter@icloud.com>
2021-03-05 18:55:16 -0700

    completed user story 2

--- app/controllers/bulk_discounts_controller.rb
+++ app/controllers/bulk_discounts_controller.rb
@@ -1,9 +1,9 @@
 class BulkDiscountsController < ApplicationController
-  before_action :three_closest_holidays, only: [:index]
 
   def index
     @merchant = Merchant.find(params[:merchant_id])
     @discounts = @merchant.bulk_discounts
+    @three_closest_holidays = HolidaySearch.new.three_closest_holidays
   end
 
   def show
