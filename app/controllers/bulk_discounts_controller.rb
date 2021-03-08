fa537c9b43c43ec2be6de4db2dd98c382c7444c1
Trevor Suter <Trevorsuter@icloud.com>
2021-03-07 20:40:43 -0700

    refactoring in the controller

--- app/controllers/bulk_discounts_controller.rb
+++ app/controllers/bulk_discounts_controller.rb
@@ -1,22 +1,21 @@
 class BulkDiscountsController < ApplicationController
+  before_action :find_merchant, except: [:show, :new]
+  before_action :find_bulk_discount, except: [:index, :new, :create]
 
   def index
-    @merchant = Merchant.find(params[:merchant_id])
     @discounts = @merchant.bulk_discounts
     @three_closest_holidays = HolidaySearch.new.three_closest_holidays
   end
 
   def show
-    @discount = BulkDiscount.find(params[:id])
   end
 
   def new
   end
 
   def create
-    @merchant = Merchant.find(params[:merchant_id])
-    bulk_discount = @merchant.bulk_discounts.new(new_bulk_discount_params)
-    if bulk_discount.save
+    @bulk_discount = @merchant.bulk_discounts.new(new_bulk_discount_params)
+    if @bulk_discount.save
       redirect_to merchant_bulk_discounts_path(@merchant)
     else
       flash[:notice] = "Bulk Discount not created: Required information missing."
@@ -25,13 +24,9 @@ class BulkDiscountsController < ApplicationController
   end
 
   def edit
-    @discount = BulkDiscount.find(params[:id])
-    @merchant = Merchant.find(params[:merchant_id])
   end
 
   def update
-    @discount = BulkDiscount.find(params[:id])
-    @merchant = Merchant.find(params[:merchant_id])
     if @discount.update(edit_bulk_discount_params)
       flash[:notice] = 'Bulk Discount Updated!'
       redirect_to merchant_bulk_discount_path(@merchant, @discount)
@@ -42,8 +37,8 @@ class BulkDiscountsController < ApplicationController
   end
 
   def destroy
-    BulkDiscount.find(params[:id]).destroy
-    redirect_to merchant_bulk_discounts_path
+    @discount.destroy
+    redirect_to merchant_bulk_discounts_path(@merchant)
   end
 
   private
@@ -54,5 +49,15 @@ class BulkDiscountsController < ApplicationController
   private
   def new_bulk_discount_params
     params.permit(:description, :percentage, :threshold)
+  end
+
+  private
+  def find_merchant
+    @merchant = Merchant.find(params[:merchant_id])
+  end
+
+  private
+  def find_bulk_discount
+    @discount = BulkDiscount.find(params[:id])
   end
 end
