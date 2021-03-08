43f3244bcb172f48b3cd90788ebcd9229a696b58
Trevor Suter <Trevorsuter@icloud.com>
2021-03-07 20:26:19 -0700

    user story 6 done

--- app/controllers/bulk_discounts_controller.rb
+++ app/controllers/bulk_discounts_controller.rb
@@ -15,7 +15,7 @@ class BulkDiscountsController < ApplicationController
 
   def create
     @merchant = Merchant.find(params[:merchant_id])
-    bulk_discount = @merchant.bulk_discounts.new(bulk_discount_params)
+    bulk_discount = @merchant.bulk_discounts.new(new_bulk_discount_params)
     if bulk_discount.save
       redirect_to merchant_bulk_discounts_path(@merchant)
     else
@@ -29,13 +29,30 @@ class BulkDiscountsController < ApplicationController
     @merchant = Merchant.find(params[:merchant_id])
   end
 
+  def update
+    @discount = BulkDiscount.find(params[:id])
+    @merchant = Merchant.find(params[:merchant_id])
+    if @discount.update(edit_bulk_discount_params)
+      flash[:notice] = 'Bulk Discount Updated!'
+      redirect_to merchant_bulk_discount_path(@merchant, @discount)
+    else
+      flash[:notice] = 'You messed up, hotshot... try again.'
+      redirect_to edit_merchant_bulk_discount_path(@merchant, @discount)
+    end
+  end
+
   def destroy
     BulkDiscount.find(params[:id]).destroy
     redirect_to merchant_bulk_discounts_path
   end
 
   private
-  def bulk_discount_params
+  def edit_bulk_discount_params
+    params.require(:bulk_discount).permit(:description, :percentage, :threshold)
+  end
+
+  private
+  def new_bulk_discount_params
     params.permit(:description, :percentage, :threshold)
   end
 end
