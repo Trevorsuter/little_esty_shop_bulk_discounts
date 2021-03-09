a48debd0353ad413b5d2eee743252f1d67ca056e
Trevor Suter <Trevorsuter@icloud.com>
2021-03-09 15:55:15 -0700

    finished user story 9

--- spec/features/admin/invoices/show_spec.rb
+++ spec/features/admin/invoices/show_spec.rb
@@ -15,8 +15,9 @@ describe 'Admin Invoices Index Page' do
 
     @ii_1 = InvoiceItem.create!(invoice_id: @i1.id, item_id: @item_1.id, quantity: 12, unit_price: 2, status: 0)
     @ii_2 = InvoiceItem.create!(invoice_id: @i1.id, item_id: @item_2.id, quantity: 6, unit_price: 1, status: 1)
-    @ii_3 = InvoiceItem.create!(invoice_id: @i2.id, item_id: @item_2.id, quantity: 87, unit_price: 12, status: 2)
+    @ii_3 = InvoiceItem.create!(invoice_id: @i2.id, item_id: @item_2.id, quantity: 90, unit_price: 12, status: 2)
 
+    @discount1 = @m1.bulk_discounts.create!(description: "discount1", threshold: 5, percentage: 5)
     visit admin_invoice_path(@i1)
   end
 
@@ -53,9 +54,13 @@ describe 'Admin Invoices Index Page' do
     expect(page).to_not have_content(@ii_3.status)
   end
 
-  it 'should display the total revenue the invoice will generate' do
+  it 'should display the total revenue the invoice will generate with the calculations' do
+    expect(page).to have_content("Gross Revenue: #{@i1.gross_revenue}")
+    expect(page).to have_content("Total Discount: -#{@i1.total_discount}")
     expect(page).to have_content("Total Revenue: $#{@i1.total_revenue}")
 
+    expect(page).to_not have_content(@i2.gross_revenue)
+    expect(page).to_not have_content(@i2.total_discount)
     expect(page).to_not have_content(@i2.total_revenue)
   end
 
